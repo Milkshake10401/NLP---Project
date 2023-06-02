fb078b8b04e4c610e6770b19a9743ef2847f92a8
Abhishek Sriram <abhisheksriram16@gmail.com>
2023-06-01 20:48:04 -0700

    Updt Script

--- NLP_production_2.py
+++ NLP_production_2.py
@@ -1,19 +1,29 @@
 import pandas as pd
 import spacy
-from spacy import displacy
+from collections import Counter
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 
 def csv_column_one(df):
     nlp = spacy.load("en_core_web_sm")
     root_list = []
+    # word_cloud = WordCloud(collocations=False, background_color='white').generate(root_list)
+    # Display the generated Word Cloud
+    # plt.imshow(word_cloud, interpolation='bilinear')
+    # plt.axis("off")
+    # plt.show()
 
     for item in df.iloc[:, 0]:
         str_item = str(item)
-        doc = nlp(str_item)
+        lower_str_item = str_item.lower()
+        doc = nlp(lower_str_item)
 
         for token in doc:
             if token.dep_ == 'ROOT':
-                root_list.append(token.text)
+                root_list.append(token.lemma_)
+
+    return print(root_list)
 
 
 def csv_column_two(df):
@@ -21,6 +31,7 @@ def csv_column_two(df):
     root_list = []
 
     for item in df.iloc[:, 1]:
+        item = item.dropna()
         str_item = str(item)
         doc = nlp(str_item)
 
@@ -35,7 +46,7 @@ def csv_column_three(df):
     nlp = spacy.load("en_core_web_sm")
     root_list = []
 
-    for item in df.iloc[:, 0]:
+    for item in df.iloc[:, 2]:
         str_item = str(item)
         doc = nlp(str_item)
 
